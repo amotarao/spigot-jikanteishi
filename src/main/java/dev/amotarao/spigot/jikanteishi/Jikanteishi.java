2a82121377bccdd8912669f69c9c984185adedd4
Amon Sawamura <amon.sawamura@gmail.com>
2021-03-28 21:05:20 +0900

    setup project

---
+++ src/main/java/dev/amotarao/spigot/jikanteishi/Jikanteishi.java
@@ -0,0 +1,16 @@
+package dev.amotarao.spigot.jikanteishi;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Jikanteishi extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
